040c7bc65f4071c580daa10270ac57dcd2a56fb9
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2024-12-13 22:41:44 +0530

    after 7 hour completion

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+  // Configuration
+  cloudinary.config({ 
+    cloud_name: 'codewithlokesh', 
+    api_key: '716996454136422', 
+    api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+});
